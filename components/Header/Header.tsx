c3623c1fab3a05e3f6dfae6b234ea274fc87a263
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-02-04 18:12:03 +0200

    Fix header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -13,7 +13,7 @@ function Header() {
             <Link href="/">Home</Link>
           </li>
           <li>
-            <Link href="/notes">Notes</Link>
+            <Link href="/notes/filter">Notes</Link>
           </li>
         </ul>
       </nav>
