e1908e0995adf93132d35591854cefaa39df0da1
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-02-05 14:12:29 +0200

    Add sidebar

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 function SidebarNotes() {
   return (
     <ul className={css.menuList}>
-      {/* список тегів */}
       <li className={css.menuItem}>
         <Link href={`/notes/filter/all`} className={css.menuLink}>
           All notes
@@ -16,6 +15,38 @@ function SidebarNotes() {
           className={css.menuLink}
         >
           Todo
+        </Link>
+      </li>
+      <li className={css.menuItem}>
+        <Link
+          href={`/notes/filter/Work`}
+          className={css.menuLink}
+        >
+          Work
+        </Link>
+      </li>
+      <li className={css.menuItem}>
+        <Link
+          href={`/notes/filter/Personal`}
+          className={css.menuLink}
+        >
+          Personal
+        </Link>
+      </li>
+      <li className={css.menuItem}>
+        <Link
+          href={`/notes/filter/Meeting`}
+          className={css.menuLink}
+        >
+          Meeting
+        </Link>
+      </li>
+      <li className={css.menuItem}>
+        <Link
+          href={`/notes/filter/Shopping`}
+          className={css.menuLink}
+        >
+          Shopping
         </Link>
       </li>
     </ul>
